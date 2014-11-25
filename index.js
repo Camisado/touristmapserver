c38164f659427d950bc9a35223e1fbe02f36745a
Camisado <YG-92@yandex.ru>
2014-11-25 10:14:17 +0300

    listen port

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello Heroku");
     response.end();
-}).listen(8888);
+})..listen(process.env.PORT || 5000);
 console.log("server started.");
